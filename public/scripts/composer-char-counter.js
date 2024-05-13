ea24724f790aeca53cb8fc17a26c7697d3c3a1b7
Somto Ufondu <somtoufondu@gmail.com>
2024-05-13 16:12:38 -0600

    Add new js file to register event for the textarea element an update the counter.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,26 @@
+// composer-char-counter.js
+$(document).ready(function () {
+  // Set character limit
+  charLimit = 140;
+
+  // Register an event handler for the textarea element
+  $("#tweet-text").on("input", function () {
+    const input = this.value;
+    const inputLength = input.length;
+
+    // Find the counter element within the parent container of the textarea
+    let counterElement = $(this).closest(".tweet-form").find(".counter");
+
+    // Calculate the number of characters left
+    const charsLeft = charLimit - inputLength;
+
+    // Update the text content of the counter element
+    counterElement.text(charsLeft);
+
+    if (charsLeft < 0) {
+      counterElement.addClass("exceeded");
+    } else {
+      counterElement.removeClass("exceeded");
+    }
+  });
+});
