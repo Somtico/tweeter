89083d2cd886b3ba05f8cfec0e4cc60e1bebe62a
Somto Ufondu <somtoufondu@gmail.com>
2024-05-17 07:50:59 -0600

    Inside the client.js file and within the document ready function, define a function called loadTweets that is responsible for fetching tweets from the http://localhost:8080/tweets page. Remove the hard-coded tweets object. Include the timeago library script from CDN in the index.html and format the tweetData.created_at to display the time passed since a tweet.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,29 +6,54 @@ $(document).ready(function () {
     // Create the main article element for tweet
     let $tweet = $("<article>").addClass("tweet");
 
+    //////////////////////////////////
     // Create the header section
+    //////////////////////////////////
     const $header = $("<header>");
+
+    // Create the avatar section
     const $span = $("<span>").addClass("tweet-avatar");
     const $avatar = $("<img>").attr("src", tweetData.user.avatars);
     const $name = $("<p>").text(tweetData.user.name);
-    $span.append($avatar, $name); // Insert avatar and name into the span element
-    const $handle = $("<p>").addClass("handle").text(tweetData.user.handle);
-    $header.append($span, $handle); // Insert span children and handle into header
 
+    // Add avatar and name to the DOM
+    $span.append($avatar, $name);
+
+    // Create the handle
+    const $handle = $("<p>").addClass("handle").text(tweetData.user.handle);
+
+    // Add span children and handle to the DOM
+    $header.append($span, $handle);
+
+    //////////////////////////////////
     // Create the content section
+    //////////////////////////////////
     const $content = $("<p>").addClass("content").text(tweetData.content.text);
 
+    //////////////////////////////////
     // Create the footer section
+    //////////////////////////////////
     const $footer = $("<footer>");
-    const $timeAgo = $("<p>").text(tweetData.created_at);
+    // Use timeago.js format function directly to get "time ago" formatted string
+    const formattedTimeAgo = timeago.format(tweetData.created_at);
+
+    // Create a <time> element with the 'timeago' class, and set its text to the formatted "time ago" string
+    const $timeAgo = $("<time>").addClass("timeago").text(formattedTimeAgo);
+
+    // Create the icon section
     const $icons = $("<div>").addClass("icons");
     const $flagIcon = $("<i>").addClass("fa-solid fa-flag");
     const $repeatIcon = $("<i>").addClass("fa-solid fa-repeat");
     const $heartIcon = $("<i>").addClass("fa-solid fa-heart");
-    $icons.append($flagIcon, $repeatIcon, $heartIcon); // Insert the icons into the div element
-    $footer.append($timeAgo, $icons); // Insert the time of tweet and icons into the footer
 
-    $tweet.append($header, $content, $footer); // Insert the header, content and footer into the article element
+    // Add the icons to the DOM
+    $icons.append($flagIcon, $repeatIcon, $heartIcon);
+
+    // Add the time of tweet and icons to the DOM
+    $footer.append($timeAgo, $icons);
+
+    // Add the header, content and footer to the DOM
+    $tweet.append($header, $content, $footer);
 
     return $tweet;
   };
@@ -39,34 +64,6 @@ $(document).ready(function () {
       $(".all-tweets").prepend($tweet);
     });
   };
-
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
-  renderTweets(data);
 
   // Attach an event listener to the form for posting new tweets
   $("#tweet-form").on("submit", function (event) {
@@ -86,4 +83,21 @@ $(document).ready(function () {
       },
     });
   });
+
+  // Function to load tweets from the server
+  const loadTweets = function () {
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      method: "GET",
+      success: function (data) {
+        renderTweets(data);
+      },
+      error: function (error) {
+        console.log("Error loading tweets:", error);
+      },
+    });
+  };
+
+  // Initial load of tweets
+  loadTweets();
 });
